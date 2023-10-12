deb5e2bff7afdd91f79a871d32f999c7c03c8a3d
feliciaL3 <113386223+feliciaL3@users.noreply.github.com>
2023-10-12 12:15:30 +0300

    5

---
+++ lab5/lab5hw/client.py
@@ -0,0 +1,133 @@
+import threading
+import socket
+import json
+import os
+import uuid
+
+
+class ChatClient:
+    def __init__(self, host, port):
+        self.HOST = host
+        self.PORT = port
+        self.client_id = str(uuid.uuid4())  # generate a unique client ID
+        self.name = input("Enter your name: ").strip()
+        self.room = input("Enter the room: ").strip()
+        self.CLIENT_DIR = os.path.join('Downloads', f'{self.name}_{self.room}')  # create a client-specific download directory
+
+        os.makedirs(self.CLIENT_DIR, exist_ok=True)
+
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.HOST, self.PORT))
+        print(f"Connected to {self.HOST}:{self.PORT}")
+
+        self.connect_to_server()
+
+        self.receive_thread = threading.Thread(target=self.receive_messages)
+        self.receive_thread.daemon = True
+        self.receive_thread.start()
+
+    def connect_to_server(self):
+        # send a connection message to the server
+        connect_msg = {
+            "type": "connect",
+            "payload": {"name": self.name, "room": self.room}
+        }
+        self.send_message(connect_msg)
+
+    def send_message(self, message):
+        try:
+            message_json = json.dumps(message).encode('utf-8')
+            self.client_socket.send(message_json)
+        except Exception as e:
+            print(f"Error sending message: {e}")
+
+    def receive_messages(self):
+        file_name = None
+        is_file = False
+        while True:
+            try:
+                message = self.client_socket.recv(1024)
+                if not message:
+                    print("Connection lost.")
+                    break
+
+                if is_file:
+                    # receiving a file
+                    with open(os.path.join(self.CLIENT_DIR, file_name), 'ab') as file:
+                        file.write(message)
+
+                        while True:
+                            self.client_socket.settimeout(0.5)
+                            try:
+                                next_chunk = self.client_socket.recv(1024)
+                                if not next_chunk:
+                                    break
+                                file.write(next_chunk)
+                            except socket.timeout:
+                                break
+
+                    print(f"File {file_name} has been downloaded successfully to {os.path.join(self.CLIENT_DIR, file_name)}.")
+                    file_name = None
+                    is_file = False
+                    self.client_socket.settimeout(None)
+                    continue
+
+                message_str = message.decode('utf-8', errors='ignore')
+
+                if message_str.strip().startswith('{') and message_str.strip().endswith('}'):
+                    message_json = json.loads(message_str)
+                    msg_type = message_json.get('type')
+
+                    if msg_type == 'file_info':
+                        file_name = message_json['payload']['name']
+                        is_file = True
+                    elif msg_type == 'notification':
+                        print(message_json['payload']['message'])
+                    elif msg_type == 'message':
+                        payload = message_json['payload']
+                        print(f"{payload['sender']}: {payload['text']}")
+                    elif msg_type == 'connect_ack':
+                        print(message_json['payload']['message'])
+                    elif msg_type == 'error':
+                        print(message_json['payload']['message'])
+
+            except Exception as e:
+                print(f"Error occurred: {e}")
+                break
+
+    def run(self):
+        try:
+            while True:
+                text = input()
+                if text.lower() == 'exit':
+                    break
+                elif text.lower().startswith('upload:'):
+                    # uploading a file
+                    _, file_path = text.split(':', 1)
+                    message = {
+                        "type": "upload",
+                        "payload": {"path": file_path.strip()}
+                    }
+                elif text.lower().startswith('download:'):
+                    # requesting a file download
+                    _, file_name = text.split(':', 1)
+                    message = {
+                        "type": "download",
+                        "payload": {"name": file_name.strip()}
+                    }
+                else:
+                    message = {
+                        "type": "message",
+                        "payload": {"sender": self.name, "room": self.room, "text": text}
+                    }
+                self.send_message(message)
+        except KeyboardInterrupt:
+            print("\nExiting the client.")
+        finally:
+            self.client_socket.close()
+
+if __name__ == "__main__":
+    HOST = '127.0.0.1'
+    PORT = 8080
+    chat_client = ChatClient(HOST, PORT)
+    chat_client.run()
---
+++ lab5/lab5hw/server.py
@@ -0,0 +1,154 @@
+import threading
+import socket
+import json
+import os
+
+HOST = '127.0.0.1'
+PORT = 8080
+ServerMedia = 'ServerMedia'
+
+if not os.path.exists(ServerMedia):
+    os.makedirs(ServerMedia)
+
+clients = []
+rooms = {}
+
+
+def format_message(msg_type, payload):
+    try:
+        message = {"type": str(msg_type), "payload": payload}
+        return json.dumps(message)
+    except (TypeError, ValueError) as e:
+        print(f"Error formatting message: {e}")
+        return None
+
+
+def send_error_message(client_socket, error_message):
+    error_msg = format_message('error', {'message': error_message})
+    client_socket.send(error_msg.encode('utf-8'))
+
+
+def upload_file(client_socket, file_path, client_name, client_room):
+    try:
+        if not os.path.exists(file_path):
+            send_error_message(client_socket, f"File {file_path} doesn't exist.")
+            return
+
+        user_folder = os.path.join(ServerMedia, f'{client_name}_{client_room}')
+        os.makedirs(user_folder, exist_ok=True)
+
+        with open(file_path, 'rb') as file:
+            content = file.read()
+            file_name = os.path.basename(file_path)
+            server_file_path = os.path.join(user_folder, file_name)
+
+            with open(server_file_path, 'wb') as server_file:
+                server_file.write(content)
+
+        notification = format_message("notification", {"message": f"User {client_name} uploaded the {file_name} ."})
+        client_socket.send(notification.encode('utf-8'))
+    except Exception as e:
+        print(f"Error occurred during file upload: {e}")
+
+
+def download_file(client_socket, file_name, client_name, client_room):
+    try:
+        user_folder = os.path.join(ServerMedia, f'{client_name}_{client_room}')
+        file_path = os.path.join(user_folder, file_name)
+
+        if not os.path.exists(file_path):
+            send_error_message(client_socket, f"The {file_name} doesn't exist.")
+            return
+
+        file_info = format_message('file_info', {'name': file_name})
+        client_socket.send(file_info.encode('utf-8'))
+
+        with open(file_path, 'rb') as file:
+            content = file.read(1024)
+            while content:
+                client_socket.send(content)
+                content = file.read(1024)
+    except Exception as e:
+        print(f"Error occurred during file download: {e}")
+
+
+def handle_client(client_socket, client_address):
+    global clients, rooms
+    client_name = ""
+    client_room = ""
+
+    while True:
+        try:
+            message_json = client_socket.recv(1024).decode('utf-8')
+            if not message_json:
+                break
+
+            message_data = json.loads(message_json)
+            print(f"Received: {message_json}")
+
+            if message_data["type"] == "connect":
+                client_name = message_data["payload"]["name"]
+                client_room = message_data["payload"]["room"]
+                clients_in_room = rooms.get(client_room, [])
+                clients_in_room.append(client_socket)
+                rooms[client_room] = clients_in_room
+
+                ack_message = format_message("connect_ack", {"message": "Connected to the room."})
+                client_socket.send(ack_message.encode('utf-8'))
+                print(f"{client_name} connected to room {client_room}")
+
+                notification = format_message("notification", {"message": f"{client_name} has joined the room."})
+                for client in clients_in_room:
+                    if client != client_socket:
+                        client.send(notification.encode('utf-8'))
+
+            elif message_data["type"] == "message":
+                clients_in_room = rooms.get(client_room, [])
+                broadcast_message = format_message("message", {
+                    "sender": client_name,
+                    "room": client_room,
+                    "text": message_data["payload"]["text"]
+                })
+                print(f"Message in {client_room} from {client_name}: {message_data['payload']['text']}")
+                for client in clients_in_room:
+                    client.send(broadcast_message.encode('utf-8'))
+
+            elif message_data["type"] == "upload":
+                file_path = message_data["payload"]["path"]
+                upload_file(client_socket, file_path, client_name, client_room)
+
+            elif message_data["type"] == "download":
+                file_name = message_data["payload"]["name"]
+                download_file(client_socket, file_name, client_name, client_room)
+
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            break
+
+    clients.remove(client_socket)
+    if client_room in rooms:
+        rooms[client_room].remove(client_socket)
+    client_socket.close()
+    print(f"Connection from {client_address} closed.")
+
+
+if __name__ == "__main__":
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((HOST, PORT))
+        server_socket.listen()
+        print(f"Server is listening on {HOST}:{PORT}")
+
+        while True:
+            client_socket, client_address = server_socket.accept()
+            clients.append(client_socket)
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+            client_thread.daemon = True
+            client_thread.start()
+    except KeyboardInterrupt:
+        print("Shutting down the server.")
+    except Exception as e:
+        print(f"Server error: {e}")
+    finally:
+        server_socket.close()
